86431f31087dc3cc93a342d03a6311c756a7f489
Michael Nguyen <michaeelnguyen@gmail.com>
2024-12-24 00:30:34 +0000

    Created baseline DAO/Service files and endpoints/handlers in Controller file.

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -16,17 +18,85 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+
+        app.post("/register", this::registerAccount);
+        app.post("/login", this::loginAccount);
+        app.post("/messages", this::createMessage);
+        app.get("/messages", this::getAllMessages);
+        app.get("/messages/{message_id}", this::getMessageByID);
+        app.delete("/messages{message_id}", this::deleteMessageByID);
+        app.patch("/messages/{message_id}", this::updateMessageByID);
+        app.get("/accounts/{account_id}/messages", this::getMessageByAccountID);
 
         return app;
     }
 
     /**
-     * This is an example handler for an example endpoint.
+     * Hander for creating a new Account for the user.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void registerAccount(Context context) throws JsonProcessingException {
+        context.json("register account");
+    }
+
+    /**
+     * Hander for login process for users.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void loginAccount(Context context) throws JsonProcessingException {
+        context.json("login account");
+    }
+
+    /**
+     * Handler for creation of new messages
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void createMessage(Context context) throws JsonProcessingException {
+        context.json("create message");
+    }
+
+    /**
+     * Handler to retrieve all messages
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    private void getAllMessages(Context context) {
+        context.json("retrieve all messages");
+    }
+
+    /**
+     * Handler to retrieve a message by its ID.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     */
+    private void getMessageByID(Context context) {
+        context.json("retrieve a message by its ID");
+    }
+
+    /**
+     * Handler to delete a message by its ID.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     */
+    private void deleteMessageByID(Context context) {
+        context.json("delete a message by its ID");
+    }
+
+    /**
+     * Handler to update a message by its ID.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     * @throws JsonProcessingException will be thrown if there is an issue converting JSON into an object.
+     */
+    private void updateMessageByID(Context context) throws JsonProcessingException {
+        context.json("update a message by its ID");
+    }
+
+    /**
+     * Handler to retrieve all messages written by a particular user account.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     */
+    private void getMessageByAccountID(Context context) {
+        context.json("retrieve all messages written by specific account ID");
     }
 
 
